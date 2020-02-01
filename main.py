3d2ab363e94521b4e1351906820bf1157cdb597b
Abhishek Kumar <iamabhishek229313@gmail.com>
2020-02-01 08:42:42 -0800

    The voice is set to best now applying the use camera

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import cv2
 import pyttsx3
+import re
 import numpy as np
 import pytesseract 
 
-img = cv2.imread('./Capture.jpg');
+img = cv2.imread('./TextSamples/timesofindiawiki.JPG');
+cap = cv2.VideoCapture(1)
+if not cap.isOpened():
+    raise IOError("Cannot open webcam")
+while True:
+    ret, frame = cap.read()
+    cv2.imshow('Input', frame)
+
+    c = cv2.waitKey(1)
+    if c == 27:
+        break
+cap.release()
+cv2.destroyAllWindows()
+
+
 language = 'en'
 custom_config = r'--oem 3 --psm 6'
-pytesseract.image_to_string(img,config = custom_config)
+#pytesseract.image_to_string(img,config = custom_config)
 
 #Speaking Engine .
 engine = pyttsx3.init()
@@ -14,6 +29,21 @@ print(engine.getProperty('rate'))
 engine.setProperty('rate' , 150)
 voices = engine.getProperty('voices')
 engine.setProperty('voice',voices[0].id)
+
+# def rotate(image,center = None ,scale = 1.0):
+#     angle = 360 - int(re.search('(?<=Rotate: )\d+',pytesseract.image_to_osd(image)).group(0))
+#     (h,w) = image.shape[:2]
+
+#     if center is None :
+#         center = (w/2,h/2)
+    
+#     M = cv2.getRotationMatrix2D(center,angle,scale)
+#     rotated = cv2.wrapAffine(image,M,(w,h))
+    
+#     return rotated
+# rot = rotate(img)
+# cv2.imshow('rptated',rot)
+
 
 def speakSomething(text):
     engine.say(text)
@@ -38,19 +68,9 @@ def opening(image):
 def canny(image):
     return cv2.Canny(image,100,200)
 
-def deskew(image):
-    coords = np.column_stack(np.where(image>0))
-    angle = cv2.minAreaRect(coords)[-1]
-    if angle < -45:
-        angle = -(90 + angle)
-    else:
-        angle = -angle
-    
-    (h,w) = image.shape[:2]
-    center = (w //2 , h//2)
-    M = cv2.getRotationMatrix2D(center,angle , 1.0)
-    rotated = cv2.wrapAffine(image,M,(w,h),flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
-    return rotated
+gray = get_grayscale(img)
+osd = pytesseract.image_to_osd(gray)
+print(osd)
 
 
 h, w, c = img.shape
